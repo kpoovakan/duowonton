d73dea4456ad59039f63733a546419f0b606178c
Pravina Ivy <217997837+kpoovakan@users.noreply.github.com>
2026-01-25 16:02:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,25 +3,25 @@ function template() {
 }
 
 function pronouns() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1057467875/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1057467875/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
 
 function berVerbsIntro() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1063311668/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1063311668/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
 
 function meVerbs() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1067133129/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1067133129/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
 
 function adjectives() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1131329289/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1131329289/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
 
 function countThings() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1054298435/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1054298435/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
 
 function familyMembers() {
-  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1110988085/flashcards/embed?i=5j8jnu&x=1jj1" height="75%" width="100%" style="border:0"></iframe></br>`;
+  document.getElementById("output").innerHTML=`</br><iframe src="https://quizlet.com/1110988085/flashcards/embed?i=5j8jnu&x=1jj1" height="100%" width="100%" style="border:0"></iframe></br>`;
 }
